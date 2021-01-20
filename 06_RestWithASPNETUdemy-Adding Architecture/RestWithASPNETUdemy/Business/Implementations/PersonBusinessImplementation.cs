7242d7c8c00a43065b41de7667a3ae37c2345a8b
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-19 23:41:48 -0300

    adding layered architecture

---
+++ 06_RestWithASPNETUdemy-Adding Architecture/RestWithASPNETUdemy/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,44 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        readonly IPersonRepository _repository;
+
+        public PersonBusinessImplementation(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+
+        public Person FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+        public Person Crete(Person person)
+        {
+            return _repository.Crete(person);
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
