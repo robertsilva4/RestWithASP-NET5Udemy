5633e393e9ed8b236282546a402d76e73101d588
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-19 21:04:14 -0300

    Adding CRUD

--- 04_RestWithASPNETUdemy-Adding Database/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ 04_RestWithASPNETUdemy-Adding Database/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -1,4 +1,5 @@
 ﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,62 +10,79 @@ namespace RestWithASPNETUdemy.Services.Implementations
 {
     public class PersonServiceImplementation : IPersonService
     {
-        private volatile int count;
+        private MySQLContext _context;
 
-        public Person Crete(Person person)
+        public PersonServiceImplementation(MySQLContext context)
         {
-            return person;
-        }
-
-        public void Delete(long id)
-        {
-            
+            _context = context;
         }
 
         public List<Person> FindAll()
         {
-
-            List<Person> persons = new List<Person>();
-            for (int i = 0; i < 8; i ++)
-            {
-                Person person = MockPerson(i);
-                persons.Add(person);
-            }
-            return persons;
+            return _context.Persons.ToList();
         }
 
 
         public Person FindByID(long id)
         {
-            return new Person
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+        public Person Crete(Person person)
+        {
+            try
             {
-                Id = IncrementAndGet(),
-                FistName = "Robert",
-                LastName = "Silva",
-                Address = "São Roque - São Paulo - Brasil",
-                Gender = "Male"
-            };
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return person;
         }
 
         public Person Update(Person person)
         {
+            if (!Exists(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
             return person;
         }
-        private Person MockPerson(int i)
+
+        public void Delete(long id)
         {
-            return new Person
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+            if (result != null)
             {
-                Id = IncrementAndGet(),
-                FistName = "Person Name" + i,
-                LastName = "Person LastName" + i,
-                Address = "Some Address" + i,
-                Gender = "Male"
-            };
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
         }
 
-        private long IncrementAndGet()
+        private bool Exists(long id)
         {
-            return Interlocked.Increment(ref count);
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
