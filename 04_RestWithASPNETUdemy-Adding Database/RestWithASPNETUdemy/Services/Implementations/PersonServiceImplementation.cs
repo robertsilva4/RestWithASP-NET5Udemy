b76af7a5add2cd48469232b5df15e113a9b08fee
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-19 21:51:36 -0300

    testing application

--- 04_RestWithASPNETUdemy-Adding Database/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ 04_RestWithASPNETUdemy-Adding Database/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -3,8 +3,6 @@ using RestWithASPNETUdemy.Model.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace RestWithASPNETUdemy.Services.Implementations
 {
