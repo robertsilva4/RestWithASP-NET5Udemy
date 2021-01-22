38c92187f288855bfaffda62bb0f30b5e01e9882
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-21 21:30:07 -0300

    adding generic repository

---
+++ 08_RestWithASPNETUdemy-Adding Generic Repository/RestWithASPNETUdemy/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,41 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        readonly IRepository<Person> _repository;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+
+        public Person FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
