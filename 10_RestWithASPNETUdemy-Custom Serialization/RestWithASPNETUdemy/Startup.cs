71827756e8e7580d5bc5ef9b3899572c14dd6d76
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-22 00:57:08 -0300

    Adding Serialization

---
+++ 10_RestWithASPNETUdemy-Custom Serialization/RestWithASPNETUdemy/Startup.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Business.Implementations;
+using System;
+using System.Collections.Generic;
+using RestWithASPNETUdemy.Repository;
+using Serilog;
+using RestWithASPNETUdemy.Repository.Generic;
+
+namespace RestWithASPNETUdemy
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment )
+        {
+            Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+ 
+
+            services.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionstring"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+           if (Environment.IsDevelopment())
+           {
+                MigrateDatabase(connection);
+           }
+            //Versioning API
+            services.AddApiVersioning();
+
+            //DEpendency Injection
+            services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+            services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration Failed", ex);
+                throw;
+            }
+        }
+    }
+}
