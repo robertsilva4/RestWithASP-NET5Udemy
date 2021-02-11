1d7e3745443560cae0891c87679042b4b1dd21af
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-02-10 22:56:55 -0300

    adding path verbs

---
+++ 17_RestWithASPNETUdemy-Query Params and Paged Search/RestWithASPNETUdemy/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using RestWithASPNETUdemy.Model.Base;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETUdemy.Model
+{
+    [Table("book")]
+    public class Book : BaseEntity
+    {
+        [Column("title")]
+        public string Title { get; set; }
+        [Column("author")]
+        public string Author { get; set; }
+        [Column("price")]
+        public decimal Price { get; set; }
+        [Column("launch_date")]
+        public DateTime LaunchDate { get; set; }
+    }
+}
