637f1fb5be58aa790678ffd0831d3116aa79dc36
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-03-18 18:05:00 -0300

    adding others databases

---
+++ 22_RestWithASPNETUdemy-IntegrationsWithSQLServer/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Robert', 'Male', 'Silva'),
+	(2, 'Anquiano - Itália', 'Leonardo', 'Male', 'Da Vinci'),
+	(3, 'Prbandar - India', 'Mahatma', 'Malele', 'Gandi'),
+	(4, 'Mevezu - Africa do Sul', 'Nelson', 'Male', 'Mandela');
